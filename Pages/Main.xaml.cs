ccb9760b508465c368d91197132447f193800a3f
TeHe4K <hryblackwii@gmail.com>
2026-05-02 04:21:56 +0500

    Добавить проект Airlines_Коневский

---
+++ Pages/Main.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Airlines_Коневский.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
